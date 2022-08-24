7830bbe332a6489157c2e07bdfa6af1d3d402c49
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-08-24 14:47:51 +0430

    Removed commented code

--- src/modules/axiosHelper.js
+++ src/modules/axiosHelper.js
@@ -8,25 +8,6 @@ let api = axios.create({
   },
 });
 
-// const params = {
-//   name: 'new',
-// };
-
-// const game = async () => {
-//   try {
-//     const respose = await api.post('games/', { ...params });
-//     let game = respose.data.result;
-//     game = game.split(' ');
-//     // eslint-disable-next-line prefer-destructuring
-//     game = game[3];
-//     return game;
-//   } catch (error) {
-//     return error;
-//   }
-// };
-
-// const newGame = game();
-
 api = axios.create({
   baseURL:
     'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/8o2JY5Wq5QBILFG2Pf76/',
